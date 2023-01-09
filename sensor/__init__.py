77fd8ec69493ada86d2c08ea330695e1e915d4a4
Radostin Marinov <radostinmar21@gmail.com>
2023-01-09 23:18:17 +0200

    Add requirements file

--- sensor/__init__.py
+++ sensor/__init__.py
@@ -12,5 +12,5 @@ class SensorOutput(BaseModel):
 
 def check_data():
     # humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-    temperature, humidity = random.Random().randint(0, 10000) / 100, random.Random().randint(5000, 10000) / 100
+    temperature, humidity = random.Random().randint(1000, 3500) / 100, random.Random().randint(5000, 10000) / 100
     return SensorOutput(temperature=temperature, humidity=humidity)
